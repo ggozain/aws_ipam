7430cc8778a3833632fa162a1455be657ee6e857
Gerardo Gozain <gozain@gmail.com>
2023-04-18 16:33:54 +0100

    modified:   output.tf

--- output.tf
+++ output.tf
@@ -2,3 +2,13 @@ output "ipam_id" {
   description = "The IPAM ID"
   value       = aws_vpc_ipam.main.id
 }
+
+output "public_scope_id" {
+  description = "The IPAM public scope ID"
+  value       = aws_vpc_ipam.main.public_default_scope_id
+}
+
+output "private_scope_id" {
+  description = "The IPAM private scope ID"
+  value       = aws_vpc_ipam.main.private_default_scope_id
+}
