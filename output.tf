0a55d0b419a1bea371badfd86107d0cd6e64bdfb
Gerardo Gozain <gozain@gmail.com>
2023-04-25 11:20:17 +0100

    added new output and fixed output names

--- output.tf
+++ output.tf
@@ -13,14 +13,19 @@ output "private_scope_id" {
   value       = aws_vpc_ipam.main.private_default_scope_id
 }
 
-output "ipam_parent_pool_id" {
+output "ipam_child_pool_id" {
   description = "The IPAM parent pool id"
   value       = aws_vpc_ipam_pool.child.id
 }
 
-output "ipam_parent_pool_cidr" {
+output "ipam_child_pool_cidr" {
   description = "The IPAM parent pool id"
   value       = aws_vpc_ipam_pool_cidr.child.cidr
+}
+
+output "ipam_child_pool_netmask_length" {
+  description = "The IPAM parent pool id"
+  value       = aws_vpc_ipam_pool_cidr.child.netmask_length
 }
 
 # output "ipam_child_pool_id" {
